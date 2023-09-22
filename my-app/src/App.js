bfa79c731908d3d1e3d9b04212573c99748beaac
aht33 <alexhtang03@gmail.com>
2023-09-22 14:22:10 -0500

    edit to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          heyyyy
+          this is alex's version
         </a>
       </header>
     </div>
